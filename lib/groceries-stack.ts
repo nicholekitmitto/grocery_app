4425949913e0c74c22b3c607dac24b9ff3d53708
Nichole Dwight <nicholedwight@gmail.com>
2023-03-28 20:00:58 -0600

    initial commit

--- lib/groceries-stack.ts
+++ lib/groceries-stack.ts
@@ -1,11 +1,17 @@
 import * as cdk from 'aws-cdk-lib';
 import { Construct } from 'constructs';
+import { aws_s3 as s3 } from 'aws-cdk-lib';
 // import * as sqs from 'aws-cdk-lib/aws-sqs';
 
 export class GroceriesStack extends cdk.Stack {
   constructor(scope: Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
 
+    new s3.Bucket(this, 'GroceriesBucket', {
+      versioned: true,
+      removalPolicy: cdk.RemovalPolicy.DESTROY,
+      autoDeleteObjects: true
+    });
     // The code that defines your stack goes here
 
     // example resource
