4710db6d4e03187cb7eb4d87dedf50dea1938a41
Nichole Dwight <nicholedwight@gmail.com>
2023-03-28 21:33:36 -0600

    created a vpc, lambda, and started the api gateway

--- lib/groceries-stack.ts
+++ lib/groceries-stack.ts
@@ -1,21 +1,55 @@
 import * as cdk from 'aws-cdk-lib';
+import {aws_s3 as s3} from 'aws-cdk-lib';
 import { Construct } from 'constructs';
-import { aws_s3 as s3 } from 'aws-cdk-lib';
-// import * as sqs from 'aws-cdk-lib/aws-sqs';
 
 export class GroceriesStack extends cdk.Stack {
   constructor(scope: Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
 
-    new s3.Bucket(this, 'GroceriesBucket', {
+    new s3.Bucket(this, 'CDKBucket', {
       versioned: true,
       removalPolicy: cdk.RemovalPolicy.DESTROY,
       autoDeleteObjects: true
     });
-    // The code that defines your stack goes here
+
+     const vpc = new cdk.aws_ec2.Vpc(this, "Vpc", {
+      subnetConfiguration: [
+        {
+          cidrMask: 24,
+          name: 'Ingress',
+          subnetType: cdk.aws_ec2.SubnetType.PRIVATE_ISOLATED,
+        }
+      ]
+    });
+
+    const handler = new cdk.aws_lambda.Function(this, "Lambda", { 
+      runtime: cdk.aws_lambda.Runtime.NODEJS_16_X,
+      code: new cdk.aws_lambda.AssetCode("resources"),
+      handler: "index.hello_world",
+      vpc: vpc,
+      vpcSubnets:
+        {
+          subnetType: cdk.aws_ec2.SubnetType.PRIVATE_ISOLATED                                                                                                               
+        }
+    });
+
+    const api = new cdk.aws_apigateway.LambdaRestApi(this, 'groceriesApi', {
+      handler: handler,
+      proxy: false
+    });
+
+    const items = api.root.addResource('items');
+    items.addMethod('GET');
+    items.addMethod('POST');
+
+    const item = items.addResource('{item}');
+    item.addMethod('GET');
+    item.addMethod('DELETE');
+
+    
 
     // example resource
-    // const queue = new sqs.Queue(this, 'GroceriesQueue', {
+    // const queue = new sqs.Queue(this, 'HelloCdkQueue', {
     //   visibilityTimeout: cdk.Duration.seconds(300)
     // });
   }
